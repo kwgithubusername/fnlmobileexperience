95758cc4a0b611a2e5ea86ac3b9b3adf6d4060bb
woudini <wukl011@gmail.com>
2015-03-19 20:10:12 -0500

    add acaccount

--- FNLApp/FirstViewController.m
+++ FNLApp/FirstViewController.m
@@ -10,20 +10,134 @@
 #import "STTwitter.h"
 #import "FLFTwitterTableViewCell.h"
 #import "FLFTwitterDataSource.h"
-
+#import <Accounts/Accounts.h>
+#import <Social/Social.h>
 #define FLFUsername @"thefunlyfe_"
 #define FLFConsumerKey @"Wdk3Vcbhbrcu7AM6EeMPTdjm5"
 #define FLFConsumerSecret @"iT6eLSTD16RidjYpJxIBbzWwnACbjc9qoLVd8iL0C7S66DBTFY"
 
 @interface FirstViewController ()
+
+typedef void (^FavoriteTweetBlock)(FLFTwitterTableViewCell *cell, NSString *idString);
+
 @property (nonatomic) NSMutableArray *twitterFeedMutableArray;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic) NSString *currentIDString;
 @property (nonatomic) FLFTwitterDataSource *dataSource;
+@property (nonatomic) STTwitterAPI *twitter;
+@property (nonatomic, copy) FavoriteTweetBlock favoriteBlock;
 
 @end
 
 @implementation FirstViewController
+
+-(void)setupTweetButtonBlocks
+{
+    __weak FirstViewController *weakSelf = self;
+    
+    void (^favoriteTweetBlock)(FLFTwitterTableViewCell*, NSString*) = ^(FLFTwitterTableViewCell *cell, NSString *idString) {
+        if (![cell.tintColor isEqual:[UIColor redColor]])
+        {
+            [weakSelf.twitter postFavoriteCreateWithStatusID:idString includeEntities:@1 successBlock:^(NSDictionary *status) {
+                cell.tintColor = [UIColor redColor];
+            } errorBlock:^(NSError *error) {
+                NSLog(@"error favoriting:%@", [error localizedDescription]);
+            }];
+        }
+        else
+        {
+            [weakSelf.twitter postFavoriteDestroyWithStatusID:idString includeEntities:@1 successBlock:^(NSDictionary *status) {
+                cell.tintColor = [UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0];
+            } errorBlock:^(NSError *error) {
+                NSLog(@"error unfavoriting:%@", [error localizedDescription]);
+            }];
+        }
+    };
+    
+    void (^retweetBlock)(FLFTwitterTableViewCell*, NSString*) = ^(FLFTwitterTableViewCell *cell, NSString *idString) {
+
+        [weakSelf.twitter postStatusRetweetWithID:idString successBlock:^(NSDictionary *status) {
+                cell.favoriteButton.enabled = NO;
+                cell.tintColor = [UIColor redColor];
+            } errorBlock:^(NSError *error) {
+                NSLog(@"error favoriting:%@", [error localizedDescription]);
+        }];
+    };
+    
+}
+
+-(void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (actionSheet.tag == 0)
+    {
+        
+    }
+}
+
+- (IBAction)tweetButtonTapped:(UIButton *)sender
+{
+    FLFTwitterTableViewCell *cell = (FLFTwitterTableViewCell *)sender.superview.superview;
+    NSInteger row = [self.tableView indexPathForCell:cell].row;
+    NSDictionary *tweetDictionary = [self.twitterFeedMutableArray objectAtIndex:row];
+    NSString *idString = tweetDictionary[@"id_str"];
+    NSLog(@"idstring is %@",idString);
+    
+    if ([sender.titleLabel.text isEqualToString:@"Favorite"])
+    {
+        
+    }
+    
+    if ([sender.titleLabel.text isEqualToString:@"Retweet"])
+    {
+        
+    }
+    
+    if ([sender.titleLabel.text isEqualToString:@"Reply"])
+    {
+        UIActionSheet *share = [[UIActionSheet alloc] initWithTitle:@"Reply" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:@"Tweet", nil];
+        [share showInView:self.view];
+    }
+    
+}
+
+-(void)setupTwitterTimeline
+{
+    ACAccountStore *account = [[ACAccountStore alloc] init];
+    ACAccountType *accountType = [account accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    [account requestAccessToAccountsWithType:accountType options:nil completion:^(BOOL granted, NSError *error) {
+        if (granted == YES)
+        {
+            NSArray *arrayOfAccounts = [account accountsWithAccountType:accountType];
+            if ([arrayOfAccounts count] > 0)
+            {
+                ACAccount *twitterAccount = [arrayOfAccounts lastObject];
+                NSURL *requestAPI = [NSURL URLWithString:@"https://api.twitter.com/1.1/statuses/user_timeline.json"];
+                NSMutableDictionary *parameters = [[NSMutableDictionary alloc] init];
+                [parameters setObject:FLFUsername forKey:@"screen_name"];
+                [parameters setObject:@"20" forKey:@"count"];
+                
+                SLRequest *posts = [SLRequest requestForServiceType:SLServiceTypeTwitter requestMethod:SLRequestMethodGET URL:requestAPI parameters:parameters];
+                posts.account = twitterAccount;
+                [posts performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error)
+                {
+                    NSArray *tweetArray = [NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingMutableContainers error:&error];
+                    if ([tweetArray count] > 0)
+                    {
+                        [self.twitterFeedMutableArray addObjectsFromArray:tweetArray];
+                        dispatch_async(dispatch_get_main_queue(),
+                        ^{
+                            [self.tableView reloadData];
+                        });
+                    }
+                }];
+                
+            }
+        } else {
+            NSLog(@"%@", [error localizedDescription]);
+        }
+    }
+     ];
+}
 
 -(void)setupDataSource
 {
@@ -107,31 +221,44 @@
 
 - (void)loadTwitter
 {
-    STTwitterAPI *twitter = [STTwitterAPI twitterAPIAppOnlyWithConsumerKey:FLFConsumerKey consumerSecret:FLFConsumerSecret];
-    
-    [twitter verifyCredentialsWithSuccessBlock:^(NSString *username)
-     {
-         [twitter getUserTimelineWithScreenName:FLFUsername successBlock:^(NSArray *statuses)
-          {
-              
-              self.twitterFeedMutableArray = [[NSMutableArray alloc] initWithArray:statuses];
-              NSLog(@"%@", self.twitterFeedMutableArray);
-              [self.tableView reloadData];
-              self.currentIDString = [self.twitterFeedMutableArray lastObject][@"id_str"];
-              
-          } errorBlock:^(NSError *error)
-          {
-              
-              NSLog(@"%@", error.debugDescription);
-              
-          }];
-         
-     } errorBlock:^(NSError *error)
-     {
-         
-         NSLog(@"%@", error.debugDescription);
-         
-     }];
+    ACAccountStore *account = [[ACAccountStore alloc] init];
+    ACAccountType *accountType = [account accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    [account requestAccessToAccountsWithType:accountType options:nil completion:^(BOOL granted, NSError *error) {
+        if (granted == YES)
+        {
+            NSArray *arrayOfAccounts = [account accountsWithAccountType:accountType];
+            if ([arrayOfAccounts count] > 0)
+            {
+                ACAccount *twitterAccount = [arrayOfAccounts lastObject];
+                self.twitter = [STTwitterAPI twitterAPIOSWithAccount:twitterAccount];
+                //    STTwitterAPI *twitter = [STTwitterAPI twitterAPIAppOnlyWithConsumerKey:FLFConsumerKey consumerSecret:FLFConsumerSecret];
+                
+                [self.twitter verifyCredentialsWithSuccessBlock:^(NSString *username)
+                 {
+                     [self.twitter getUserTimelineWithScreenName:FLFUsername successBlock:^(NSArray *statuses)
+                      {
+                          
+                          self.twitterFeedMutableArray = [[NSMutableArray alloc] initWithArray:statuses];
+                          NSLog(@"%@", self.twitterFeedMutableArray);
+                          [self.tableView reloadData];
+                          self.currentIDString = [self.twitterFeedMutableArray lastObject][@"id_str"];
+                          
+                      } errorBlock:^(NSError *error)
+                      {
+                          
+                          NSLog(@"%@", error.debugDescription);
+                          
+                      }];
+                     
+                 } errorBlock:^(NSError *error)
+                 {
+                     
+                     NSLog(@"%@", error.debugDescription);
+                     
+                 }];
+            }
+        }
+    }];
 }
 
 
