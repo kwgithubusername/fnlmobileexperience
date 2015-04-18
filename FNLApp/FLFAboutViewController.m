05e7ec2b2cad0541aa3e21ead90618e1a1f67fb9
woudini <wukl011@gmail.com>
2015-04-17 21:53:54 -0500

    add about section and present modally to avoid unnecessary headache from adding navigation controller

---
+++ FNLApp/FLFAboutViewController.m
@@ -0,0 +1,24 @@
+//
+//  FLFAboutViewController.m
+//  FNLApp
+//
+//  Created by Woudini on 4/17/15.
+//  Copyright (c) 2015 Hi Range. All rights reserved.
+//
+
+#import "FLFAboutViewController.h"
+
+@interface FLFAboutViewController ()
+
+@end
+
+@implementation FLFAboutViewController
+
+- (IBAction)backButtonClicked:(UIBarButtonItem *)sender
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+
+@end
