cb79bb4f2799403068b1e0b825e463af9eb12c14
woudini <wukl011@gmail.com>
2015-04-26 20:53:05 -0500

    fix dateformatter and add tests

--- FNLApp/FLFDateFormatter.m
+++ FNLApp/FLFDateFormatter.m
@@ -18,16 +18,17 @@
     if(timeSinceDate < 24.0 * 60.0 * 60.0)
     {
         NSUInteger hoursSinceDate = (NSUInteger)(timeSinceDate / (60.0 * 60.0));
-        
+        NSUInteger minutesSinceDate = (NSUInteger)(timeSinceDate / 60);
         switch(hoursSinceDate)
         {
             default:
-                return [NSString stringWithFormat:@"%luh", hoursSinceDate];
+                return [NSString stringWithFormat:@"%luh", (unsigned long)hoursSinceDate];
             case 0:
                 // x minutes ago
-                return (timeSinceDate / 60.0) == 0 ? [[NSString alloc] initWithFormat:@"%lus",(NSUInteger)(timeSinceDate)] :
-                [[NSString alloc] initWithFormat:@"%lum",(NSUInteger)(timeSinceDate / 60.0)];
-            break;
+                return minutesSinceDate == 0 ?
+                [[NSString alloc] initWithFormat:@"%lus",(unsigned long)(timeSinceDate)] :
+                [[NSString alloc] initWithFormat:@"%lum",(unsigned long)(minutesSinceDate)];
+                break;
         }
     }
     else
@@ -43,16 +44,16 @@
             if (weeksSinceDate >= 52)
             {
                 NSUInteger yearsSinceDate = (NSUInteger)(timeSinceDate/60/60/24/7/52);
-                return [[NSString alloc] initWithFormat:@"%luy", yearsSinceDate];
+                return [[NSString alloc] initWithFormat:@"%luy", (unsigned long)yearsSinceDate];
             }
             else
             {
-                return [[NSString alloc] initWithFormat:@"%luw", weeksSinceDate];
+                return [[NSString alloc] initWithFormat:@"%luw", (unsigned long)weeksSinceDate];
             }
         }
         else
         {
-            return [[NSString alloc] initWithFormat:@"%lud", daysSinceDate];
+            return [[NSString alloc] initWithFormat:@"%lud", (unsigned long)daysSinceDate];
         }
     }
 }
